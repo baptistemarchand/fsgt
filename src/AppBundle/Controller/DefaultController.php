6251eecea7ac18374a0546e10d692afd35c55dac
Baptiste Marchand <baptistemarchand42@gmail.com>
2017-07-21 22:29:42 +0200

    bootstrap <3

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,17 +18,32 @@ class DefaultController extends Controller
     {
         $user = $this->getUser();
 
-        if ($user->payment_status === 'paid' && $user->getMedicalCertificateName())
+        if ($user->status === 'waiting_documents' && $user->payment_status === 'paid' && $user->getMedicalCertificateName())
         {
             $user->status = $user->skill_checked ? 'member' : 'waiting_skill_check';
             $em = $this->get('doctrine')->getManager();
             $em->persist($user);
             $em->flush();
         }
+
+        $em = $this->get('doctrine')->getManager();
+        $users = $em->getRepository(User::class)->findAll();
+        $repartition = [
+            'new' => 0,
+            'in_lottery' => 0,
+            'waiting_for_documents' => 0,
+            'waiting_skill_check' => 0,
+            'member' => 0,
+            'in_waiting_list' => 0,
+        ];
+
+        foreach ($users as $u)
+            $repartition[$u->status] += 1;
         
         return $this->render('default/index.html.twig', [
             'user' => $user,
-            'club' => $this->get('doctrine')->getRepository(Club::class)->find(1),
+            'club' => $this->get('doctrine')->getRepository(Club::class)->find(1), // 1 == 3MQ
+            'repartition' => $repartition,
         ]);
     }
     
