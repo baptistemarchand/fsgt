2fc00c3964a1cbf162e28dfc6e4ab546283a63b9
Baptiste Marchand <baptistemarchand42@gmail.com>
2017-08-18 20:17:20 +0200

    Fix EasyAdmin route

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -4,9 +4,17 @@ declare(strict_types=1);
 namespace AppBundle\Controller;
 
 use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class AdminController extends BaseAdminController
 {
+    /** @Route("/admin/", name="easyadmin") */
+    public function indexAction(Request $request)
+    {
+        return parent::indexAction($request);
+    }
+
     public function createNewUserEntity()
     {
         return $this->get('fos_user.user_manager')->createUser();
